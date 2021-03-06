20263e30e8c1d8d0aaced9dae681fa505039d984
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-03-06 16:12:06 -0700

    finished layout and styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import './App.css';
+import {useState} from "react"
 
-import mainImage from "./scrimba-404.svg"
+import './styles/App.css';
+
+import mainImage from "./images/scrimba-404.svg"
+import scrimbaLogo from "./images/logo.svg"
+import blobRight from "./images/blobs/blob-right.svg"
+import blobRightKevin from "./images/blobs/blob-right-kevin.svg"
+import blobLeft from "./images/blobs/blob-left.svg"
+import blobLeftKevin from "./images/blobs/blob-left-kevin.svg"
+import blobBottom from "./images/blobs/blob-bottom.svg"
+import blobBottomKevin from "./images/blobs/blob-bottom-kevin.svg"
 
 
 function App() {
+  const [click, setClick] = useState(true)
   return (
-    <div className="App">
-    <div className="container">
-    <img src={mainImage} className="main-image" />
-    <div className="header">Oh uh</div>
-    </div>
+    <div className="App-container">
+      <img className="blob-right" src={click ? blobRight : blobRightKevin} alt="blob" />
+      <img className="blob-left" src={click ? blobLeft : blobLeftKevin}  alt="blob" />
+      <img className="blob-bottom"  onClick={() => setClick(!click)} src={click ? blobBottom : blobBottomKevin}  alt="blob" />
+  
+
+      <a href="https://scrimba.com/" className="nav">
+        <img className="logo" src={scrimbaLogo} alt="logo"/>
+        <div className="scrimba">scrimba</div>
+        <div className="btn">Resume course</div>
+      </a>
+
+      <div className="image-container">
+        <img src={mainImage} className="main-image" alt="404 page" />
+        <div className="header">Oops!</div>
+        <div className="subtitle">the page your looking for no longer exist</div>
+        <div className="easter-egg-message">(unless your looking for a Kevin Powell easter egg... in that case, happy searching!)</div>
+      </div>
    
     </div>
   );
